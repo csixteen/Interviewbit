ee904d342479d31cca35040c0ddf1f8e34e87029
Pedro Rodrigues <csixteen@protonmail.com>
2019-10-13 10:06:32 +0100

    Subset

---
+++ Backtracking/subsets.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# coding: utf-8
+import unittest
+
+
+class Solution:
+    def subsets(self, A):
+        res = [[]]
+        for c in A:
+            res += list(map(lambda x: sorted(x + [c]), res))
+
+        return sorted(res)
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.test_cases = [
+            ([], [[]]),
+            ([1], [[], [1]]),
+            ([1, 2], [[], [1], [2], [1, 2]]),
+            ([1, 2, 3], [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]),
+            ([1, 2, 3, 4], [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3], [4], [1, 4], [2, 4], [1, 2, 4], [3, 4], [1, 3, 4], [2, 3, 4], [1, 2, 3, 4]])
+        ]
+
+    def test_subsets(self):
+        s = Solution()
+        for i, e in self.test_cases:
+            self.assertEqual(s.subsets(i), e)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
